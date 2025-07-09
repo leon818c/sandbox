1c05a34bfbf8b1e963e182a2278bff7b14dcc178
Leon Thomas <leonthomas2020@gmail.com>
2025-07-08 22:06:44 -0500

    extra changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://sudefkjvakmdgcvyzjao.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1ZGVma2p2YWttZGdjdnl6amFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NDU4OTMsImV4cCI6MjA2NzUyMTg5M30.jfRS38VFOHAM7bXts8ZVSYebEiNTmdyPaRYmAMnAZTc'
+  };
+  
